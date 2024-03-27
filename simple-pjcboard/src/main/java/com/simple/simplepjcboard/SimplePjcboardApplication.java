76ec7a56bd7b905134af84d4be6c5f5b7186dda4
devcatly <dpfls66@gmail.com>
2024-03-27 16:28:06 +0900

    make a

---
+++ simple-pjcboard/src/main/java/com/simple/simplepjcboard/SimplePjcboardApplication.java
@@ -0,0 +1,13 @@
+package com.simple.simplepjcboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePjcboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePjcboardApplication.class, args);
+    }
+
+}
---
+++ simple-pjcboard/src/test/java/com/simple/simplepjcboard/SimplePjcboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.simplepjcboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePjcboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
